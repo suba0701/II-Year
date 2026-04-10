bba2c859865e807695a5b6f4c3f309e6be03d476
suba0701 <subalaxmik07@gmail.com>
2026-04-10 12:25:20 +0530

    Create car.py

---
+++ Python/Solved/Class/car.py
@@ -0,0 +1,13 @@
+
+class Car:
+    def drive(self) -> None:
+        print("Vroom!")
+
+
+def main() -> None:
+    my_car = Car()
+    my_car.drive()  # Vroom!
+
+
+if __name__ == "__main__":
+    main()
